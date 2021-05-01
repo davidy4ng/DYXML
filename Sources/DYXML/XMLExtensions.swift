a3361c8b5e75510ed09f6cfd5bb7c64926c804f6
David Yang <david.yang@insideapp.fr>
2021-05-01 13:24:51 +0200

    fix XMLExtensions accessibility

--- Sources/DYXML/XMLExtensions.swift
+++ Sources/DYXML/XMLExtensions.swift
@@ -1,13 +1,13 @@
 extension XML {
     /// Returns the XML as a string. No indentation or new lines will be inserted. Is the equivalent of calling `toString(withIndentation: nil)`
-    var string: String {
+    public var string: String {
         return self.toString()
     }
 
     /// Returns the XML as a string with indentation and new lines
     /// - Parameter space: the number of spaces a level of indentation. If space is `nil`, no indentation or new lines will be inserted.
     /// - Returns: the content XML as a string
-    func toString(withIndentation space: Int? = nil) -> String {
+    public func toString(withIndentation space: Int? = nil) -> String {
         let xmlOutputStreamString = XMLOutputStreamString(indentation: space)
         renderXML(into: xmlOutputStreamString)
         return xmlOutputStreamString.output
